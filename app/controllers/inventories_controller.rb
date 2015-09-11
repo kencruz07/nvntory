b4a789f39083ce5fb243ff1676044580db57f2ba
kencruz07 <kencruz07@gmail.com>
2015-09-11 10:07:33 +0800

    Added working ajax call for creating inventories, improved UI

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,12 +1,25 @@
 class InventoriesController < ApplicationController
 
-  def new
+  def index
+    @inventories = Inventory.all
     @inventory = Inventory.new
   end
 
   def create
     @inventory = Inventory.new inventory_params
-    raise "#{@inventory.name}, #{@inventory.category}, #{@inventory.quantity}"
+    # raise "#{@inventory.name}, #{@inventory.category}, #{@inventory.quantity}"
+    respond_to do |format|
+      if @inventory.save
+        # UserMailer.notification(@comment.user,@comment.content).deliver_now
+        format.html { redirect_to :back}
+        format.js { render :layout => false }
+        format.json { render :json => @inventory, :status => :created }
+      else
+        format.html { redirect_to :back }
+        format.json { render :json => @inventory.errors,
+          :status => :unprocessable_entity }
+      end
+    end
   end
 
   def inventory_params
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,5 @@
 class Inventory < ActiveRecord::Base
+
+  scope :list_by_id, -> { order :id => :asc }
+
 end
